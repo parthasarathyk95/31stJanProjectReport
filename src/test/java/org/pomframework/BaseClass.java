7b3b33db84c251244f1119560dd1537445d6fdec
karthi <parthasarathyk95@gmail.com>
2022-02-01 15:40:58 +0530

    new function added in base class

--- src/test/java/org/pomframework/BaseClass.java
+++ src/test/java/org/pomframework/BaseClass.java
@@ -1,5 +1,17 @@
 package org.pomframework;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -62,11 +74,51 @@ public class BaseClass {
 	public static void pageRefresh() {
 		driver.navigate().refresh();
 	}
-//time function
+	//time function
 	public static void hold(int time) throws InterruptedException {
 		Thread.sleep(time);
 	}
-// newly added function
+	// Datadriven excel base class function
+	public static String readExcel(int rowNo,int cellNo) throws IOException {
+		File f = new File("C:\\Users\\intel\\eclipse-workspace\\Examplemaven\\excel\\testdata.xlsx");
+		FileInputStream fin = new FileInputStream(f);
+		Workbook book = new XSSFWorkbook(fin);
+		//Workbook book = new XSSFWorkbook(fin);
+		Sheet sh = book.getSheet("login9");
+		Row r = sh.getRow(rowNo);
+		//System.out.println(r);
+		Cell c = r.getCell(cellNo); 
+		//System.out.println(c);
+
+		int type = c.getCellType();
+		String name;
+		//String name1;
+		if (type == 1) {
+			name = c.getStringCellValue();
+			//System.out.println("String: ");
+			System.out.println(name);
+		}
+		else if(DateUtil.isCellDateFormatted(c)){
+			Date da = c.getDateCellValue();
+			//System.out.println(da);
+			SimpleDateFormat form = new SimpleDateFormat("dd-mm-yyyy");
+			name = form.format(da);
+			//System.out.println(name);
+
+		}
+		else {
+			double d = c.getNumericCellValue();
+			//System.out.println(d);
+			long l = (long)d;
+			//System.out.println(l);
+			name = String.valueOf(l);
+			//System.out.println("Integer as String: ");
+			System.out.println(name);
+		}
+		return name;
+		//return name1;
+	}	
+	// newly added function
 	private void new_function() {
 		System.out.println("new_function");
 	}
