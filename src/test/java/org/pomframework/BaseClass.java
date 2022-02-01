0902cf1b28c5c55c7d5fca56c737f922e666c0c4
karthi <parthasarathyk95@gmail.com>
2022-02-01 18:09:45 +0530

    today method with updated code from master

--- src/test/java/org/pomframework/BaseClass.java
+++ src/test/java/org/pomframework/BaseClass.java
@@ -20,57 +20,57 @@ import org.openqa.selenium.support.ui.Select;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
-	//
+	// 
 	public static WebDriver driver;
-//chromebrowser function
+	//chromebrowser method
 	public static void chromeBrowser() {
-	WebDriverManager.chromedriver().setup();
-	driver = new ChromeDriver();	
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();	
 	}
-//launchurl function
+	//launchurl method
 	public static void launchUrl(String url) {
 		driver.get(url);
 	}
-//mainwindow function
+	//mainwindow method
 	public static void mainwindow() {
 		driver.manage().window().maximize();
 	}	
-//pageTitle function	
+	//pageTitle method	
 	public static void pageTitle() {
 		String titleName = driver.getTitle();
 		System.out.print("titleName: ");
 		System.out.println(titleName);
 	}	
-//pageUrl function	
+	//pageUrl method	
 	public static void pageUrl() {
 		String Url = driver.getCurrentUrl();
 		System.out.print("Url: ");
 		System.out.println(Url);
 	}
-//fillTextBox function
+	//fillTextBox method with parameter Webelement and String 
 	public static void fillTextbox(WebElement element, String value) {
 		element.sendKeys(value);
 	}
-//fillTextBox function for dropdown select function
+	//dropdown method with parameter Webelement and String 
 	public static void dropdown(WebElement element, String value) {
 		Select s = new Select(element);
 		s.selectByValue(value);
 	}
-//fillTextBox function for dropdown select function
+	//dropdown method with parameter Webelement and integer 
 	public static void dropdownindex(WebElement element, int v) {
 		Select s = new Select(element);
 		s.selectByIndex(v);
 	}
-	//fillTextBox function for dropdown select function
+	//dropdowntxt method with parameter Webelement and String 
 	public static void dropdowntxt(WebElement element, String text) {
 		Select s = new Select(element);
 		s.selectByVisibleText(text);
 	}
-//btnclick function
+	//btnclick method
 	public static void btnclick(WebElement ref) {
 		ref.click();
 	}
-//pageRefresh function
+	//pageRefresh method
 	public static void pageRefresh() {
 		driver.navigate().refresh();
 	}
@@ -122,8 +122,13 @@ public class BaseClass {
 	private void new_function() {
 		System.out.println("new_function");
 	}
-	// today_method 
-	private void today_method() {
-		System.out.println("today_function");
-	}
+	// another method	
+		private void another_method() {
+			System.out.println("another_method");
+		}
+    // today_method
+		private void today_method() {
+			System.out.println("today_method");
+		}
+
 }
